b9e9c3b26b1b93d1451a3806c85c84cbe2be32f7
Rajeel Siddiqui <159783237+RajeelSiddiqui1@users.noreply.github.com>
2026-05-14 19:31:29 +0500

    Update docker_compose.groovy

--- vars/docker_compose.groovy
+++ vars/docker_compose.groovy
@@ -1,5 +1,5 @@
 def call (){
   echo 'Deploy has been started'
-  sh "docker-compose down && docker compose up -d "
+  sh "docker-compose down && docker-compose up -d "
   echo 'deploy successfully'
 }
