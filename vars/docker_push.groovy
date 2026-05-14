9549c02668ae4da9afe62d1ad390e46f9685d472
Rajeel Siddiqui <159783237+RajeelSiddiqui1@users.noreply.github.com>
2026-05-14 19:38:22 +0500

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,9 +1,16 @@
-def call(String Project, String ImageTage, String dockerHubUser){
- withCredentials([usernamePassword(credentialsId: "DockerHubCred", 
-                                 usernameVariable: 'dockerHubUser', 
-                                 passwordVariable: 'dockerHubPass')]){
-                sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-                sh "docker image tag notes-app:latest ${dockerHubUser}/${Project}:${ImageTage}"
-                sh "docker push ${dockerHubUser}/${Project}:${ImageTage}"
-}
+def call(String Project, String ImageTag, String HubUser){
+    withCredentials([usernamePassword(credentialsId: "DockerHubCred", 
+                                     usernameVariable: 'D_USER', 
+                                     passwordVariable: 'D_PASS')]){
+        
+        // Login
+        sh "echo ${D_PASS} | docker login -u ${D_USER} --password-stdin"
+        
+        // Image tag (Aapki built image 'HubUser/Project' format mein hai)
+        // Hum purani image ko naye 'Project' naam ke sath tag kar rahe hain agar zaroorat ho
+        sh "docker image tag ${HubUser}/notes-app:${ImageTag} ${D_USER}/${Project}:${ImageTag}"
+        
+        // Push to Docker Hub
+        sh "docker push ${D_USER}/${Project}:${ImageTag}"
+    }
 }
