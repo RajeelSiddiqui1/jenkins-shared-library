3e7ca9fbcc5cf2f25f00ee4bd5607336ecbfcec7
Rajeel Siddiqui <159783237+RajeelSiddiqui1@users.noreply.github.com>
2026-05-14 19:31:14 +0500

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,6 +3,7 @@ def call(String Project, String ImageTage, String dockerHubUser){
                                  usernameVariable: 'dockerHubUser', 
                                  passwordVariable: 'dockerHubPass')]){
                 sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-                sh "docker image tag notes-app:latest ${dockerHubUser}/django-note-app:latest"
+                sh "docker image tag notes-app:latest ${dockerHubUser}/${Project}:${ImageTage}"
                 sh "docker push ${dockerHubUser}/${Project}:${ImageTage}"
+}
 }
